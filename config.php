538b3f2bed47fb5eff91885683eae10fe4c7de54
pedro <pedro06bf@gmail.com>
2022-12-13 11:56:38 -0300

    testando

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbHost = 'sql.freedb.tech';
+$dbUsername = 'freedb_pedrocinema';
+$dbPassword = '3Sz7UCFQGu@N3Zr';
+$dbName = 'freedb_clubedecinema';
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+
+
+?>
