126c27de19dc07546b6d38e53c43a254a90a1753
pedro <pedro06bf@gmail.com>
2023-01-17 20:20:10 -0300

    em teoria funcionando

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 AOS.init();
 
-
 url=315162
+var nota;
+
+fetch('https://api.themoviedb.org/3/movie/'+url+'?api_key=7006774efe5e4045670045e98084b7c8')
+.then(resposta=>{
+    return resposta.json()
+}).then(corpo=>{
+    poster=corpo.poster_path
+    nome=corpo.original_title
+    nota=corpo.vote_average
+    nota=nota*10
+    document.getElementById("rate").innerHTML=Math.trunc(nota)
+    document.getElementById("titulo-filme").innerHTML=nome
+    document.getElementById("poster").src="https://image.tmdb.org/t/p/original" + poster
+
+});
+
+fetch('https://api.themoviedb.org/3/movie/'+url+'/videos?api_key=7006774efe5e4045670045e98084b7c8')
+.then(pegartrailer=>{
+    return pegartrailer.json()
+}).then(trailer=>{
+    console.log(trailer)
+    v=trailer.results.length-1
+    var link=(trailer.results[v].key)
+    document.getElementById("link").src= "https://www.youtube.com/embed/"+link
+})
+
+
 
 
 console.log(localStorage.url)
@@ -22,3 +48,5 @@ function logar(){
         alert('Usuário ou Senha incorretos')
     }
 }
+
+
